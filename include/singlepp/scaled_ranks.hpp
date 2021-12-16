c03e4f190b9d724f14d49a130331e3df339927b5
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2021-12-15 22:32:13 -0800

    More flexible inputs for scaled_ranks.

--- include/singlepp/scaled_ranks.hpp
+++ include/singlepp/scaled_ranks.hpp
@@ -11,25 +11,7 @@ namespace singlepp {
 
 typedef std::vector<std::pair<double, size_t> > ranked_vector;
 
-template <class Start> 
-void scaled_ranks(Start start, const std::vector<int>& chosen, ranked_vector& collected, std::vector<double>& outgoing, bool na_aware = false) {
-    size_t slen=chosen.size();
-    collected.clear();
-    collected.reserve(slen);
-
-    // Sorting all subsetted values (zeroes are handled separately for greater efficiency).
-    size_t s=0;
-    for (auto i : chosen) {
-        const double curval=*(start + i);
-        if (std::isnan(curval)) { 
-            if (!na_aware) {
-                throw std::runtime_error("missing values not supported in SingleR");
-            }
-        } else {
-            collected.push_back(std::make_pair(curval, s));
-        }
-        ++s;
-    }
+inline void scaled_ranks(size_t slen, ranked_vector& collected, std::vector<double>& outgoing, bool na_aware = false) {
     std::sort(collected.begin(), collected.end());
 
     // Computing tied ranks. 
@@ -65,7 +47,7 @@ void scaled_ranks(Start start, const std::vector<int>& chosen, ranked_vector& co
     double sum_squares=0;
     const double center_rank=static_cast<double>(collected.size() - 1)/2; // use collected.size(), not slen, to account for NA's.
     for (auto& o : outgoing) {
-        if (na_aware && ISNA(o)) {
+        if (na_aware && std;:isnan(o)) {
             continue;
         }
         o-=center_rank;
@@ -76,7 +58,7 @@ void scaled_ranks(Start start, const std::vector<int>& chosen, ranked_vector& co
     sum_squares = std::max(sum_squares, 0.00000001);
     sum_squares = std::sqrt(sum_squares)*2;
     for (auto& o : outgoing) {
-        if (na_aware && ISNA(o)) {
+        if (na_aware && std::isnan(o)) {
             continue;
         }
         o/=sum_squares;
@@ -85,6 +67,51 @@ void scaled_ranks(Start start, const std::vector<int>& chosen, ranked_vector& co
     return;
 }
 
+template <class Start> 
+void scaled_ranks(size_t slen, Start start, ranked_vector& collected, std::vector<double>& outgoing, bool na_aware = false) {
+    collected.clear();
+    collected.reserve(slen);
+
+    // Sorting all subsetted values (zeroes are handled separately for greater efficiency).
+    for (size_t s = 0; s < slen; ++s) {
+        const double curval=*(start + s);
+        if (std::isnan(curval)) { 
+            if (!na_aware) {
+                throw std::runtime_error("missing values not supported in SingleR");
+            }
+        } else {
+            collected.emplace_back(curval, s);
+        }
+    }
+
+    scaled_ranks(slen, collected, outgoing, na_aware);
 }
+
+
+template <class Start> 
+void scaled_ranks(Start start, const std::vector<int>& chosen, ranked_vector& collected, std::vector<double>& outgoing, bool na_aware = false) {
+    size_t slen=chosen.size();
+    collected.clear();
+    collected.reserve(slen);
+
+    // Sorting all subsetted values (zeroes are handled separately for greater efficiency).
+    size_t s=0;
+    for (auto i : chosen) {
+        const double curval=*(start + i);
+        if (std::isnan(curval)) { 
+            if (!na_aware) {
+                throw std::runtime_error("missing values not supported in SingleR");
+            }
+        } else {
+            collected.emplace_back(curval, s);
+        }
+        ++s;
+    }
+
+    scaled_ranks(slen, collected, outgoing, na_aware);
+}
+
+
+
 
 #endif
