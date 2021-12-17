2e76aff58dc4f53de4f5258d572443255b12b630
Aaron Lun <lun.aaron@gene.com>
2021-12-16 16:56:34 -0800

    Added more functionality for slicing markers.

--- include/singlepp/Markers.hpp
+++ include/singlepp/Markers.hpp
@@ -3,46 +3,148 @@
 
 #include <vector>
 #include <algorithm>
+#include <unordered_set>
 
 namespace singlepp {
 
 class Markers {
+    virtual ~Markers() {}
+    virtual size_t size() const = 0;
+    virtual std::pair<size_t, const int*> get(size_t, size_t) const = 0;
+
+    void combine(size_t n, const int* labels, std::vector<int>& output) const {
+        std::unordered_set<int> store;
+
+        for (size_t i = 0; i < n; ++i) {
+            for (size_t j = 0; j < n; ++j) {
+                if (i != j) {
+                    auto current = this->get(labels[i], labels[j]);
+                    output.insert(current.second, current.second + current.first);
+                }
+            }
+        }
+
+        output.clear();
+        for (auto s : store) {
+            output.push_back(s);
+        }
+        std::sort(output.begin(), output.end());
+
+        return;
+    }
+
+    void combine(std::vector<int>& output) const {
+        std::unordered_set<int> store;
+
+        for (size_t i = 0; i < n; ++i) {
+            for (size_t j = 0; j < n; ++j) {
+                if (i != j) {
+                    auto current = this->get(i, j);
+                    output.insert(current.second, current.second + current.first);
+                }
+            }
+        }
+
+        output.clear();
+        for (auto s : store) {
+            output.push_back(s);
+        }
+        std::sort(output.begin(), output.end());
+
+        return;
+    }
+
+    FullMarkers subset(size_t n, const int* subset) const {
+        std::vector<std::vector<std::vector<int> > > subsetted(this->size());
+
+        for (size_t i = 0; i < this->size(); ++i) {
+            subsetted[i].resize(this->size());
+            for (size_t j = 0; j < this->size(); ++j) {
+                if (i == j) {
+                    continue;
+                }
+
+                auto current = this->get(i, j);
+                auto& output = subsetted[i][j];
+                auto ptr = current.second;
+                size_t counter = 0;
+
+                for (size_t k = 0; k < current.first; ++k, ++ptr) {
+                    while (counter < n && subset[counter] < *ptr) {
+                        ++counter;
+                    }
+                    if (counter == n) {
+                        break;
+                    }
+                    if (subset[counter] == *ptr) {
+                        output.push(counter);
+                    }
+                }
+            }
+        }
+
+        return FullMarkers(std::move(subsetted));
+    }
+};
+
+class LightMarkers : public Markers {
 public:
-    Markers(std::vector<std::pair<size_t, int*> > m) : ranked_markers(std::move(m)) {}
+    LightMarkers(std::vector<std::vector<std::pair<size_t, const int*> > > m) : ranked_markers(std::move(m)) {}
 
     size_t size() const {
         return ranked_markers.size();
     }
 
-    const auto& operator[](size_t l) const {
-        return ranked_markers[l];        
+    std::pair<size_t, const int*> get(size_t i, size_t j) const {
+        return ranked_markers[i][j];
+    }
+private:
+    std::vector<std::vector<std::pair<size_t, const int*> > > ranked_markers;
+};
+
+class FullMarkers : public Markers {
+public:
+    FullMarkers(std::vector<std::vector<std::vector<int> > > m) : ranked_markers(std::move(m)) {}
+
+    size_t size() const {
+        return ranked_markers.size();
     }
 
-    void combine(size_t n, const int* chosen, std::vector<int>& output) {
-        output.clear();
-        for (size_t i = 0; i < n; ++i) {
-            const auto& current = ranked_markers[i];
-            output.insert(output.end(), current.second, current.second + current.first);
-        }
+    std::pair<size_t, const int*> get(size_t i, size_t j) const {
+        const auto& current = ranked_markers[i][j];
+        return std::make_pair<size_t, const int*>(current.size(), current.data());
+    }
 
-        if (output.size()) {
-            std::sort(output.begin(), output.end());
-            size_t counter = 0;
-            int last = output[0];
-            for (size_t i = 1; i < output.size(); ++i) {
-                if (output[i] != last) {
-                    output[counter] = output[i];
-                    ++counter;
-                    last = output[i];
+    void subset_in_place(size_t n, const int* subset) {
+        for (size_t i = 0; i < this->size(); ++i) {
+            for (size_t j = 0; j < this->size(); ++j) {
+                if (i == j) {
+                    continue;
+                }
+
+                auto& current = ranked_markers[i][j];
+                size_t counter = 0;
+                size_t i = 0;
+
+                for (size_t k = 0; k < current.size(); ++k) {
+                    while (counter < n && subset[counter] < current[k]) {
+                        ++counter;
+                    }
+                    if (counter == n) {
+                        break;
+                    }
+                    if (subset[counter] == current[k]) {
+                        current[i] = counter;
+                        ++i;
+                    }
                 }
             }
-            output.resize(counter);
         }
 
         return;
     }
 private:
-    std::vector<std::pair<size_t, int*> > ranked_markers;
+    std::vector<std::vector<std::vector<int> > > ranked_markers;
 };
 
 }
