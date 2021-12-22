deff014a04801ee285b494ab0226e204ae9d4487
LTLA <LTLA@users.noreply.github.com>
2021-12-22 20:46:06 +0000

    Deploying to gh-pages from @ clusterfork/singlepp@ad6acecae6b82278f518fc6a25af82e3aa1ec112 🚀

--- docs/search/all_1.js
+++ docs/search/all_1.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['best_0',['best',['../structsinglepp_1_1SinglePP_1_1Results.html#adcfecad00e22152fd96b4845ace74934',1,'singlepp::SinglePP::Results']]],
-  ['build_1',['build',['../classsinglepp_1_1SinglePP.html#a2e173014b07fd51181c094872af7ac74',1,'singlepp::SinglePP']]]
+  ['build_1',['build',['../classsinglepp_1_1SinglePP.html#ac4d206142c5eedd2cf12a500bfc10655',1,'singlepp::SinglePP']]]
 ];
--- docs/search/functions_0.js
+++ docs/search/functions_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['build_0',['build',['../classsinglepp_1_1SinglePP.html#a2e173014b07fd51181c094872af7ac74',1,'singlepp::SinglePP']]]
+  ['build_0',['build',['../classsinglepp_1_1SinglePP.html#ac4d206142c5eedd2cf12a500bfc10655',1,'singlepp::SinglePP']]]
 ];
