32a592514ba41fa32b71b4684e9a1b938f30362c
Aaron Lun <infinite.monkeys.with.keyboards@gmail.com>
2022-06-26 17:38:48 -0700

    Migrated the classic algorithm for choosing markers from SingleR. (#10)

---
+++ tests/src/ChooseClassicMarkers.cpp
@@ -0,0 +1,169 @@
+#include <gtest/gtest.h>
+#include "custom_parallel.h"
+
+#include "singlepp/ChooseClassicMarkers.hpp"
+#include "tatami/tatami.hpp"
+#include "spawn_matrix.h"
+
+class ChooseClassicMarkersTest : public ::testing::TestWithParam<std::tuple<int, bool> > {};
+
+TEST_P(ChooseClassicMarkersTest, Simple) { 
+    size_t ngenes = 100;
+    size_t nlabels = 10;
+    auto params = GetParam();
+    int requested = std::get<0>(params);
+    bool reverse = std::get<1>(params);
+
+    auto mat = spawn_matrix(ngenes, nlabels, 1234 * requested);
+    std::vector<int> groupings(nlabels);
+    std::iota(groupings.begin(), groupings.end(), 0);
+    if (reverse) { // reversing the label order to test the indexing.
+        std::reverse(groupings.begin(), groupings.end());
+    }
+
+    singlepp::ChooseClassicMarkers mrk;
+    mrk.set_number(requested);
+    auto output = mrk.run(mat.get(), groupings.data());
+
+    // Comparing against a naive implementation.
+    EXPECT_EQ(output.size(), nlabels);
+    for (size_t l = 0; l < nlabels; ++l) {
+        const auto& current = output[l];
+        EXPECT_EQ(current.size(), nlabels);
+        auto left = mat->column(reverse ? nlabels - l - 1 : l);
+
+        for (size_t l2 = 0; l2 < nlabels; ++l2) {
+            const auto& markers = current[l2];
+            int nmarkers = markers.size();
+
+            if (l == l2) {
+                EXPECT_TRUE(markers.empty());
+                continue;
+            } else if (requested >= 0) {
+                EXPECT_TRUE(nmarkers <= requested);
+            }
+
+            auto right = mat->column(reverse ? nlabels - l2 - 1 : l2);
+            std::vector<std::pair<double, int> > sorted;
+            for (size_t i = 0; i < right.size(); ++i) {
+                sorted.emplace_back(right[i] - left[i], i);
+            }
+
+            std::sort(sorted.begin(), sorted.end());
+            for (int g = 0; g < nmarkers; ++g) {
+                EXPECT_TRUE(sorted[g].first < 0);
+                EXPECT_EQ(sorted[g].second, markers[g]);
+            }
+
+            if (nmarkers < requested && nmarkers + 1 < static_cast<int>(ngenes)) {
+                EXPECT_TRUE(sorted[nmarkers].first >= 0); // early rejection.
+            }
+        }
+    }
+}
+
+TEST_P(ChooseClassicMarkersTest, Blocked) { 
+    size_t ngenes = 100;
+    size_t nlabels = 10;
+    auto params = GetParam();
+    int requested = std::get<0>(params);
+    bool reverse = std::get<1>(params);
+
+    auto mat1 = spawn_matrix(ngenes, nlabels, 1234 * requested);
+    auto mat2 = spawn_matrix(ngenes, nlabels, 5678 * requested);
+
+    std::vector<int> groupings(nlabels);
+    std::iota(groupings.begin(), groupings.end(), 0);
+    if (reverse) {
+        std::reverse(groupings.begin(), groupings.end());
+    }
+
+    singlepp::ChooseClassicMarkers mrk;
+    mrk.set_number(requested);
+    auto output = mrk.run(std::vector<const tatami::Matrix<double, int>*>{ mat1.get(), mat2.get() },
+                          std::vector<const int*>{ groupings.data(), groupings.data() });
+
+    // Summing the two matrices together.
+    std::vector<double> combined(ngenes * nlabels);
+    auto cIt = combined.begin();
+    for (size_t l = 0; l < nlabels; ++l) {
+        auto col1 = mat1->column(l);
+        auto col2 = mat2->column(l);
+        for (size_t g = 0; g < ngenes; ++g, ++cIt) {
+            *cIt = col1[g] + col2[g];
+        }
+    }
+    tatami::DenseColumnMatrix<double, int> comb_mat(ngenes, nlabels, std::move(combined));
+    auto ref = mrk.run(&comb_mat, groupings.data());
+
+    ASSERT_EQ(output.size(), ref.size());
+    ASSERT_EQ(output.size(), nlabels);
+    for (size_t l = 0; l < nlabels; ++l) {
+        ASSERT_EQ(output[l].size(), ref[l].size());
+        ASSERT_EQ(output[l].size(), nlabels);
+
+        for (size_t l2 = 0; l2 < nlabels; ++l2) {
+            EXPECT_EQ(output[l][l2], ref[l][l2]);
+        }
+    }
+}
+
+TEST_P(ChooseClassicMarkersTest, BlockedMissing) { 
+    size_t ngenes = 100;
+    size_t nlabels = 10;
+    auto params = GetParam();
+    int requested = std::get<0>(params);
+    bool reverse = std::get<1>(params);
+
+    singlepp::ChooseClassicMarkers mrk;
+    mrk.set_number(requested);
+
+    // Label 0 is missing from the first matrix.
+    auto mat1 = spawn_matrix(ngenes, nlabels - 1, 1234 * requested);
+    auto mat2 = spawn_matrix(ngenes, nlabels, 5678 * requested);
+
+    std::vector<int> groupings1(nlabels - 1);
+    std::iota(groupings1.begin(), groupings1.end(), 1);
+    if (reverse) {
+        std::reverse(groupings1.begin(), groupings1.end());
+    }
+
+    std::vector<int> groupings2(nlabels);
+    std::iota(groupings2.begin(), groupings2.end(), 0);
+
+    // Creating the two references. We remove label 0 (i.e., the first column) from mat2 to 
+    // match the groups available in mat1; we also just compute markers from mat2 alone.
+    auto sub = tatami::make_DelayedSubsetBlock<1>(mat2, 1, nlabels);
+    auto ref_comb = mrk.run(std::vector<const tatami::Matrix<double, int>*>{ mat1.get(), sub.get() },
+                            std::vector<const int*>{ groupings1.data(), groupings2.data() + 1 });               
+    
+    auto ref_solo = mrk.run(mat2.get(), groupings2.data());
+
+    // Comparing them.
+    auto output = mrk.run(std::vector<const tatami::Matrix<double, int>*>{ mat1.get(), mat2.get() },
+                          std::vector<const int*>{ groupings1.data(), groupings2.data() });
+
+    ASSERT_EQ(ref_comb.size(), output.size());
+    ASSERT_EQ(ref_solo.size(), output.size()); 
+    ASSERT_EQ(ref_comb.size(), nlabels);
+
+    for (size_t l = 0; l < nlabels; ++l) {
+        ASSERT_EQ(ref_comb[l].size(), output[l].size());
+        ASSERT_EQ(ref_solo[l].size(), output[l].size());
+        ASSERT_EQ(output[l].size(), nlabels);
+
+        for (size_t l2 = 0; l2 < nlabels; ++l2) {
+            auto& ref = ((l2 == 0 || l == 0) ? ref_solo : ref_comb);
+            EXPECT_EQ(output[l][l2], ref[l][l2]);
+        }
+    }
+}
+
+INSTANTIATE_TEST_CASE_P(
+    ChooseClassicMarkers,
+    ChooseClassicMarkersTest,
+    ::testing::Combine(
+        ::testing::Values(-1, 5, 10, 20, 100, 1000), // number of top genes.
+        ::testing::Values(false, true) // whether to reverse the label order
+    )
+);
