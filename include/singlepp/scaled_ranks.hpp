b66d0b39e02fa53f4b5b29b52def511a33a2baac
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2024-08-21 09:00:12 -0700

    Argh just switch back to an unordered_map.

--- include/singlepp/scaled_ranks.hpp
+++ include/singlepp/scaled_ranks.hpp
@@ -7,7 +7,7 @@
 #include <vector>
 #include <cmath>
 #include <limits>
-#include <cstdint>
+#include <unordered_map>
 
 namespace singlepp {
 
@@ -65,56 +65,35 @@ void scaled_ranks(const RankedVector<Stat_, Index_>& collected, Output_* outgoin
 template<typename Index_>
 class RankRemapper {
 private:
-    // Avoid the hassle of using an unordered_set when the indices have a
-    // well-defined bound at the maximum number of genes.
-    std::vector<uint8_t> my_present;
-    std::vector<Index_> my_position;
-    Index_ counter = 0;
+    std::unordered_map<Index_, Index_> my_mapping;
+    Index_ my_counter = 0;
 
 public:
-    void add(size_t i) {
-        if (i >= my_present.size()) {
-            my_present.resize(i + 1);
-            my_position.resize(i + 1);
-        }
-        if (!my_present[i]) {
-            my_present[i] = 1;
-            my_position[i] = counter;
-            ++counter;
+    void add(Index_ i) {
+        auto it = my_mapping.find(i);
+        if (it == my_mapping.end()) {
+            my_mapping[i] = my_counter;
+            ++my_counter;
         }
     }
 
     void clear() {
-        counter = 0;
-        std::fill(my_present.begin(), my_present.end(), 0);
+        my_counter = 0;
+        my_mapping.clear();
     }
 
     void reserve(size_t n) {
-        my_present.resize(n);
-        my_position.resize(n);
+        my_mapping.reserve(n);
     }
 
 public:
     template<typename Stat_>
     void remap(const RankedVector<Stat_, Index_>& input, RankedVector<Stat_, Index_>& output) const {
         output.clear();
-
-        // We need to compare the second element of each entry of input to the
-        // current size before indexing into 'my_present', but we need to avoid
-        // cases where we accidentally overflow.
-        if (my_present.size() > static_cast<size_t>(std::numeric_limits<Index_>::max())) {
-            for (const auto& x : input) {
-                if (my_present[x.second]) {
-                    output.emplace_back(x.first, my_position[x.second]);
-                }
-            }
-
-        } else {
-            Index_ limit = my_present.size();
-            for (const auto& x : input) {
-                if (x.second < limit && my_present[x.second]) {
-                    output.emplace_back(x.first, my_position[x.second]);
-                }
+        for (const auto& x : input) {
+            auto it = my_mapping.find(x.second);
+            if (it != my_mapping.end()) {
+                output.emplace_back(x.first, it->second);
             }
         }
     }
