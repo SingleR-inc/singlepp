de8ae84313b34cba416c3f4c8f5bec5e2f741698
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2026-02-13 00:29:05 +1100

    Hardened tests for sparse L2 calculations.

--- tests/src/build_reference.cpp
+++ tests/src/build_reference.cpp
@@ -3,8 +3,8 @@
 #include "singlepp/build_reference.hpp"
 
 TEST(ComputeL2, DenseDense) {
-    std::vector<double> a{1.2, -0.5, 2.3, 5.6 };
-    std::vector<double> b{-.2, -5.1, 4.4, 1.6 };
+    std::vector<double> a{ 1.2, -0.5, 2.3, 5.6 };
+    std::vector<double> b{ -.2, -5.1, 4.4, 1.6 };
 
     double expected = 0;
     for (std::size_t i = 0; i < a.size(); ++i) {
@@ -42,9 +42,9 @@ singlepp::CompressedSparseVector<int, double> dense_to_compressed_sparse(const s
     return output;
 }
 
-TEST(ComputeL2, DenseSparse) {
-    std::vector<double> a{3.2, 1.2, -2.7, -.5,   .6, 2.3, 5.6, -1.3, -2.4 };
-    std::vector<double> b{ -1, -.2,   -1,  -1, -5.1,  -1, 4.4,   -1,  1.6 };
+TEST(ComputeL2, DenseSparseEasy) {
+    std::vector<double> a{ 3.2, 1.2, -2.7, -.5,   .6, 2.3, 5.6, -1.3, -2.4 };
+    std::vector<double> b{  -1, -.2,   -1,  -1, -5.1,  -1, 4.4,   -1,  1.6 };
     const auto expected = singlepp::compute_l2(a.size(), a, b);
 
     // Sparse scaled.
@@ -77,9 +77,38 @@ TEST(ComputeL2, DenseSparse) {
     }
 }
 
-TEST(ComputeL2, SparseSparse) {
-    std::vector<double> a{3.2, 1.4, -2, -.5,   -2, 2.3,  -2, -2, -2.4};
-    std::vector<double> b{ -1, -.2, -1,  -1, -5.1,  -1, 4.4, -1,  1.6};
+TEST(ComputeL2, DenseSparseHard) {
+    std::mt19937_64 rng(12345);
+    std::normal_distribution<> ndist;
+    std::uniform_real_distribution<> udist;
+    std::size_t n = 53;
+
+    for (int it = 0; it < 100; ++it) {
+        const double default_val = it / 200.0;
+        std::vector<double> aa(n), bb(n, default_val);
+
+        for (std::size_t i = 0; i < n; ++i) {
+            aa[i] = ndist(rng);
+            if (udist(rng) <= 0.2) {
+                bb[i] = ndist(rng);
+            }
+        }
+        auto expected = singlepp::compute_l2(n, aa, bb);
+
+        auto scaled_bb = dense_to_sparse_scaled(bb, default_val);
+        EXPECT_EQ(expected, singlepp::compute_l2<int>(n, aa, scaled_bb));
+        EXPECT_EQ(expected, singlepp::compute_l2<int>(n, scaled_bb, aa));
+
+        std::vector<int> ibuffer;
+        std::vector<double> vbuffer;
+        auto cs_b = dense_to_compressed_sparse(bb, default_val, ibuffer, vbuffer);
+        EXPECT_EQ(expected, singlepp::compute_l2<int>(n, aa, cs_b));
+    }
+}
+
+TEST(ComputeL2, SparseSparseEasy) {
+    std::vector<double> a{ 3.2, 1.4, -2, -.5,   -2, 2.3,  -2, -2, -2.4 };
+    std::vector<double> b{  -1, -.2, -1,  -1, -5.1,  -1, 4.4, -1,  1.6 };
     const auto expected = singlepp::compute_l2(a.size(), a, b);
 
     {
@@ -127,5 +156,43 @@ TEST(ComputeL2, SparseSparse) {
         EXPECT_FLOAT_EQ(singlepp::compute_l2(static_cast<int>(a.size()), sparse_empty, a), expected);
 
         EXPECT_FLOAT_EQ(singlepp::compute_l2(static_cast<int>(a.size()), sparse_empty, sparse_empty), 0);
+    }
+}
+
+TEST(ComputeL2, SparseSparseHard) {
+    std::mt19937_64 rng(12345);
+    std::normal_distribution<> ndist;
+    std::uniform_real_distribution<> udist;
+    std::size_t n = 57;
+
+    for (int it = 0; it < 100; ++it) {
+        const double default_val_a = it / 250.0;
+        const double default_val_b = it / 150.0;
+        std::vector<double> aa(n, default_val_a), bb(n, default_val_b);
+
+        for (std::size_t i = 0; i < n; ++i) {
+            aa[i] = ndist(rng);
+            if (udist(rng) <= 0.25) {
+                bb[i] = ndist(rng);
+            }
+        }
+
+        for (std::size_t i = 0; i < n; ++i) {
+            aa[i] = ndist(rng);
+            if (udist(rng) <= 0.25) {
+                bb[i] = ndist(rng);
+            }
+        }
+        auto expected = singlepp::compute_l2(n, aa, bb);
+
+        auto scaled_aa = dense_to_sparse_scaled(aa, default_val_a);
+        auto scaled_bb = dense_to_sparse_scaled(bb, default_val_b);
+        EXPECT_EQ(expected, singlepp::compute_l2<int>(n, scaled_aa, scaled_bb));
+
+        std::vector<int> ibuffer_a, ibuffer_b;
+        std::vector<double> vbuffer_a, vbuffer_b;
+        auto cs_a = dense_to_compressed_sparse(aa, default_val_a, ibuffer_a, vbuffer_a);
+        auto cs_b = dense_to_compressed_sparse(bb, default_val_b, ibuffer_b, vbuffer_b);
+        EXPECT_EQ(expected, singlepp::compute_l2<int>(n, cs_a, cs_b));
     }
 }
